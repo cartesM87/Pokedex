1c9db86deebe95dc69bb30259da81127555aa3b2
sebastian cartes <carteszero@gmail.com>
2023-02-16 10:54:18 -0400

    mejora del header y se creo menu desplegable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,36 @@
-import React,{lazy,Suspense} from 'react'
-import { Link} from 'react-router-dom'
-import {FaSearch} from "react-icons/fa"
-import Searcher from './Searcher'
+import React,{lazy,Suspense,useState} from 'react'
+import { Link} from 'react-router-dom';
+import {GoThreeBars} from "react-icons/go";
 
 function Header() {
 
-  const Img = lazy(()=>import("./Image"))
+  const Img = lazy(()=>import("./Image"));
+
+  const [display, setDisplay] = useState(false);
+  let controllerD=()=>{display?setDisplay(false):setDisplay(true)};
 
   return (
-    <header className=' grid-cols-1 place-items-center grid bg-gray-700 w-full py-2'>
-        <Suspense fallback={<span className='loader'></span>}>
-          <Link className='px-6' to={"/"}>
-              <Img url={"/src/assets/pokelogo.png"} alt={"Logo"} clas={"img-shadow w-[120px] "} />
-          </Link>   
-        </Suspense> 
-        <div className='flex items-center'>
-          <nav className='flex  px-4 gap-3'>
-              <Link className='hover:text-yellow-500 transition duration-75 text-white font-semibold'>link1</Link>
-              <Link className='hover:text-yellow-500 transition duration-75 text-white font-semibold'>link2</Link>
-              <Link className='hover:text-yellow-500 transition duration-75 text-white font-semibold'>link3</Link>
-              <Link className='hover:text-yellow-500 transition duration-75 text-white font-semibold'>link4</Link>   
-          </nav>
-          <Link to={"/searcher"} className='text-lg col-span-1'><FaSearch /></Link>
-        </div>
+    <header >
+        <div className='bg-gray-800  flex justify-evenly items-center md:grid w-full py-2'>
+          <button onClick={controllerD} className='md:hidden block col-span-1'>
+            <GoThreeBars className='text-white text-2xl'/>
+          </button> 
+          <Suspense  fallback={<span className='loader'></span>}>
+            <Link className='px-6' to={"/"}>
+                <Img url={"/src/assets/pokelogo.png"} alt={"Logo"} clas={"img-shadow w-[120px] "} />
+            </Link>   
+          </Suspense> 
+          <span></span>
 
+        </div>
+        <nav style={display?{display:"flex"}:{display:"none"}} className='flex md:hidden bg-gray-700 flex-col justify-center items-center px-4 gap-3'>
+            <Link className='md:hidden transition duration-75 text-white font-semibold'>link1</Link>
+            <Link className='md:hidden transition duration-75 text-white font-semibold'>link2</Link>
+            <Link className='md:hidden transition duration-75 text-white font-semibold'>link3</Link>
+            <Link className='md:hidden transition duration-75 text-white font-semibold'>link4</Link>   
+        </nav>
     </header>
+
     )
 }
 
